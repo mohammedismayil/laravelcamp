24c7fb144315d8181eacd924f09e4b236c326935
mohammedismayil <mohammedismayil10@gmail.com>
2021-04-22 00:05:15 +0530

    there is nothing wrong when u are sticking with basics , basics are everything

--- app/Http/Controllers/usersignupController.php
+++ app/Http/Controllers/usersignupController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\user_lists;
 class usersignupController extends Controller
 {
     //
 
 
     function register(Request $request){
-        dd($request->all());  //to check all the datas dumped from the form
+        // dd($request->all());  //to check all the datas dumped from the form
         //if your want to get single element,someName in this case
         // echo("register tapped");
+
+        $user = new user_lists();
+        $user->email = request('email');
+        $user->password = request('password') ;
+        $user->check_me = request('check_me') == "on" ? true :  false;
+        $user->save();
+
     }
 }
--- app/Models/user_lists.php
+++ app/Models/user_lists.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class usersignup extends Model
+class user_lists extends Model
 {
     use HasFactory;
 
